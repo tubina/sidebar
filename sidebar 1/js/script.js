1b08f26b81bd4c02d1fc5dfedbb9a602436403bc
Felipe Pereira <felipe.fpp@hotmail.com>
2024-05-26 16:24:22 -0300

    first

---
+++ sidebar 1/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('open_btn').addEventListener('click', function(){
+    console.log('Glória a Deus');
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+})
